a944350523622fdd023bcefc74a2aa40acb3fa11
nicole <thiswayq@gmail.com>
2016-08-07 18:53:00 -0300

    added new things

--- app/src/main/java/com/example/nicole/justjava/MainActivity.java
+++ app/src/main/java/com/example/nicole/justjava/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.nicole.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int numberOfCoffees = 0;
@@ -22,32 +23,44 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = numberOfCoffees * 5;
-        String priceMessage = "Total: R$" + price + ",00";
-        priceMessage = priceMessage + "\n" +
-                "Obrigada pelo pedido! :)";
-        displayMessage(priceMessage);
+        String email = createOrderSumary(calculatePrice());
+        EditText name = (EditText) findViewById(R.id.name);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava App order for " + name.getText());
+        intent.putExtra(Intent.EXTRA_TEXT, email);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 
     public void increment(View view) {
         numberOfCoffees = numberOfCoffees + 1;
+        if(numberOfCoffees > 100){
+            numberOfCoffees = 100;
+            Toast.makeText(getApplicationContext(), "Muito café faz mal! ):", Toast.LENGTH_SHORT).show();
+        }
         display(numberOfCoffees);
-        displayPrice(numberOfCoffees*5);
     }
 
     public void decrement(View view) {
         numberOfCoffees = numberOfCoffees - 1;
-        if (numberOfCoffees < 0 ) {
-            numberOfCoffees = 0;
+        if (numberOfCoffees < 1 ) {
+            numberOfCoffees = 1;
+            Toast.makeText(getApplicationContext(), "Não vai beber nada? ):", Toast.LENGTH_SHORT).show();
         }
         display(numberOfCoffees);
-        displayPrice(numberOfCoffees*5);
     }
 
     public void reset(View view) {
         numberOfCoffees = 0;
         display(0);
-        displayPrice(0);
+        CheckBox whipedCream = (CheckBox) findViewById(R.id.whipped_cream);
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
+        EditText name = (EditText) findViewById(R.id.name);
+        whipedCream.setChecked(false);
+        chocolate.setChecked(false);
+        name.setText("");
     }
 
     /**
@@ -59,13 +72,36 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private boolean hasWhippedCream(){
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whipped_cream);
+        return whippedCream.isChecked();
     }
 
-    public void displayMessage(String message){
-        TextView price = (TextView) findViewById(R.id.price);
-        price.setText(message);
+    private boolean hasChocolate(){
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
+        return chocolate.isChecked();
+    }
+
+    public int calculatePrice(){
+        int toppings = 0;
+
+        if (hasWhippedCream()) {
+            toppings += 1;
+        }
+        if (hasChocolate()){
+            toppings += 2;
+        }
+        return (numberOfCoffees * 5) + (numberOfCoffees * toppings);
+    }
+
+    public String createOrderSumary(int price){
+        EditText view_name = (EditText) findViewById(R.id.name);
+        String name = view_name.getText().toString();
+        return getString(R.string.order_sum_name, name) +
+                "\n" + getString(R.string.has_whipped_cream) + " ? " + hasWhippedCream() +
+                "\n" + getString(R.string.has_chocolate) + " ? " + hasChocolate() +
+                "\n"+ getString(R.string.quantity) + ": " + numberOfCoffees +
+                "\nTotal: R$" + price +
+                "\n" + getString(R.string.thank_you);
     }
 }
